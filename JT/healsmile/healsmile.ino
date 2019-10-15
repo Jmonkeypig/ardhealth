4694612d7ba24e4ca0db0e4e26d8b4c811b16c14
Jmonkeypig <jmonkeypig@gmail.com>
2019-10-15 09:45:40 +0900

    코드 수정

--- JT/healsmile/healsmile.ino
+++ JT/healsmile/healsmile.ino
@@ -1,10 +1,21 @@
-#include<Wire.h>
-#include <OzOLED.h>
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <SPI.h>
+#include <Wire.h>
+
+
+#include "OLED_Driver.h"
+#include "OLED_GUI.h"
+#include "DEV_Config.h"
+#include "Show_Lib.h"
+#include "Debug.h"
 
 const int MPU = 0x68;  //MPU 6050 의 I2C 기본 주소
 int16_t AcX,AcY,AcZ, Tmp, GyX,GyY,GyZ;
 
+//U8G2_SSD1327_WS_128X128_1_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 12, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 7, /* reset=*/ 8);//
 
+  
 #define SYSTEM_STATUS_FLOAT 10
 #define SYSTEM_STATUS_ERROR 20
 #define SYSTEM_STATUS_READY 30
@@ -13,28 +24,59 @@ int16_t AcX,AcY,AcZ, Tmp, GyX,GyY,GyZ;
 
 #define PLAY_TIMEOUT 29000
 
+
 long int end_check_time = -1;
 int system_status = SYSTEM_STATUS_FLOAT;
 int inputPin = 2;   // choose input pin 7 for the push button
 long count = 0;
+char charCount ="";
 long timer = 0;
 long gyroval = 0;
 bool checkCount = true;
+
+
+extern OLED_DIS sOLED_DIS;
 void setup() {
-
+  System_Init();
   pinMode(inputPin, INPUT); // declare push button as input
-
-  OzOled.init();  //initialze OLED display
-
-  OzOled.clearDisplay();           //clear the screen and set start position to top left corner
-  OzOled.setNormalDisplay();       //Set display to Normal mode
-  OzOled.setPageMode();            //Set addressing mode to Page Mode
-//  OzOled.printNumber((long)123, 0, 0);
-//  OzOled.printNumber((long)0xFFFF, 0, 0); 
-//  OzOled.printNumber((long)0xFFFFFFFF, 2, 0);
   count = -1;
-  OzOled.printNumber(count, 3, 0);
 
+  Serial.println("OLED Init ");
+  OLED_SCAN_DIR OLED_ScanDir = SCAN_DIR_DFT;
+  OLED_Init( OLED_ScanDir );
+
+  Serial.println("OLED_Show ");
+  //GUI_Show();
+  OLED_ClearBuf();
+  OLED_ClearScreen(OLED_BACKGROUND);
+
+  GUI_DrawLine(0, 8, sOLED_DIS.OLED_Dis_Column - 1, 8, WHITE, LINE_SOLID , DOT_PIXEL_DFT);
+  OLED_DisPage(0, 0);
+  OLED_DisPage(0, 7);
+  OLED_ClearBuf();
+
+
+  GUI_DisString_EN(30, 2, "ELECTRONIC", &Font12, FONT_BACKGROUND, WHITE);
+  OLED_DisPage(0, 2);
+  OLED_ClearBuf();
+
+//  GUI_DrawCircle(30,10,5,WHITE,DRAW_FULL,1);
+//
+//  GUI_Disbitmap(0  , 2, Signal816  , 16, 8);
+//  GUI_Disbitmap(24 , 2, Bluetooth88, 8 , 8);
+//  GUI_Disbitmap(40 , 2, Msg816     , 16, 8);
+//  GUI_Disbitmap(64 , 2, GPRS88     , 8 , 8);
+//  GUI_Disbitmap(90 , 2, Alarm88    , 8 , 8);
+  GUI_Disbitmap(112, 2, Bat816     , 16, 8);
+  OLED_Display(0, 52, 128, 52 + 16);
+  OLED_ClearBuf();
+
+  GUI_DisString_EN(0 , 0, "MUSIC", &Font12, FONT_BACKGROUND, WHITE);
+  GUI_DisString_EN(48, 0, "MENU" , &Font12, FONT_BACKGROUND, WHITE);
+  GUI_DisString_EN(90, 0, "PHONE", &Font12, FONT_BACKGROUND, WHITE);
+  
+  OLED_Display(0, 96, 128, 96 + 16);
+  OLED_ClearBuf();
   
   Wire.begin();      //Wire 라이브러리 초기화
   Wire.beginTransmission(MPU); //MPU로 데이터 전송 시작
@@ -43,67 +85,68 @@ void setup() {
   Wire.endTransmission(true);
   
   Serial.begin(9600);
+  Serial.println("serial 9600 begin");
+
   
   system_status = SYSTEM_STATUS_READY;
-
-  Serial.println("serial 9600 begin");
-  system_status = SYSTEM_STATUS_READY;
-
   Serial.println("system ready");
  
 }
 
-void loop() {
 
-  //ready
+
+uint8_t draw_state = 0;
+
+
+
+void loop() {
+  
   if (system_status == SYSTEM_STATUS_READY) {
       delay(100);
       
-      
 
-//      end_check_time = millis();
     
       int pushed = digitalRead(inputPin);  // read input value
 
-      if (pushed == LOW) { //not push button
-//        Serial.println("LOW");
-      }
-
       if (pushed == HIGH) { //push button
         count = 0;
-        OzOled.printNumber(count, 6, 5); //count print
         Serial.println("HIGH");
 
         pinMode(inputPin, INPUT_PULLUP);
         delay(1000);
         Serial.println("3");
         timer = 3;
-        OzOled.printNumber(timer, 3, 0);
 
         delay(1000);
         Serial.println("2");
         timer = 2;
-        OzOled.printNumber(timer, 3, 0);
         delay(1000);
         timer = 1;
-        OzOled.printNumber(timer, 3, 0);
         Serial.println("1");
         delay(1000);
         count = 0;
-        OzOled.printNumber(count, 3, 0);
         system_status = SYSTEM_STATUS_PLAYING;
         
+      }else{
+        
       }
+      
     
   } else if (system_status == SYSTEM_STATUS_PLAYING) {//game playing -> count
+    
     int pushed = digitalRead(inputPin);  // read input value
 
     if (inputPin == LOW) {
-      Serial.println("HIGH");
+      Serial.println("LOW");
       system_status = SYSTEM_STATUS_READY;
     }
     
+
+
+    
     delay(100);
+
+   
     Wire.beginTransmission(MPU);    //데이터 전송시작
     Wire.write(0x3B);               // register 0x3B (ACCEL_XOUT_H), 큐에 데이터 기록
     Wire.endTransmission(false);    //연결유지
@@ -130,20 +173,28 @@ void loop() {
     Serial.println(val_y);
     gyroval = val_y;
    
-    OzOled.printNumber(gyroval, 3, 0);
    
-  
+    
     if(gyroval < -70){
       if(checkCount == true){
           checkCount = false;
+          
           count = count +1;
-          OzOled.printNumber(count, 6, 5);        
+         
+          
+          GUI_DisString_EN(30, 2, "O", &Font12, FONT_BACKGROUND, WHITE);
+          OLED_DisPage(0, 2);
+          OLED_ClearBuf();
       }
+     
     }
 
      if(gyroval > -10){
       if(checkCount == false){
           checkCount = true;
+          GUI_DisString_EN(30, 2, "X", &Font12, FONT_BACKGROUND, WHITE);
+          OLED_DisPage(0, 2);
+          OLED_ClearBuf();
       }
     }
 
@@ -180,4 +231,7 @@ void loop() {
 //      system_status = SYSTEM_STATUS_END;
 //    }
 //  }
+
+
+
 }
